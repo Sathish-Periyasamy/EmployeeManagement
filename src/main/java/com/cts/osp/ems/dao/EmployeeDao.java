3595d1ffc774416920a21a2f07373d62c3cb36d6
Sathish-Periyasamy <p.sathish@outlook.com>
2017-04-03 17:27:08 +0530

    Update EmployeeDao.java

--- src/main/java/com/cts/osp/ems/dao/EmployeeDao.java
+++ src/main/java/com/cts/osp/ems/dao/EmployeeDao.java
@@ -13,28 +13,30 @@ import org.springframework.stereotype.Component;
 
 import com.cts.osp.ems.model.Employee;
 
+// This Class defines the Database transaction of Employee management system..
+
 @Component
 public class EmployeeDao {
 	
     @Autowired
 	private JdbcTemplate jdbcTemplate;  
-	  
+	
+	// This Method is used to save new Employee Details
 	public int saveEmployee(Employee e){  
 	    String query="insert into "+'"'+"Employee"+'"'+" values('"+e.getId()+"','"+e.getName()+"',"
 	    		+ "'"+e.getJoiningDate()+"','"+e.getDepartment()+"',current_timestamp,current_timestamp)"; 
 	    System.out.println(query);
-	    //jdbcTemplate.getClass();
 	    return jdbcTemplate.update(query);  
 	}  
 	
-	
+	// This Method is used to update Employee
 	public int updateEmployee(Employee e){  
-	    String query="update Employee set name='"+e.getName()+"',department='"+e.getDepartment()+"',joining_Date='"+e.getJoiningDate()+"' ,last_Modified_On=current_timestamp'"+
-	" where id='"+e.getId()+"' ";  
+	    String query="update "+'"'+"Employee"+'"'+" set name='"+e.getName()+"',department='"+e.getDepartment()+"',joining_Date='"+e.getJoiningDate()+"' ,last_Modified_On=current_timestamp'"+
+	    " where id='"+e.getId()+"' ";  
 	    return jdbcTemplate.update(query);  
 	}  
 
-	
+	// This method is used to check whether employee is present in database
 	public List<Employee> checkQuery(int employeeId){
 		String query ="Select * from"+ '"'+"Employee"+'"'+" where employee_id ='"+employeeId+"'";
 		return jdbcTemplate.query(query,new ResultSetExtractor<List<Employee>>(){  
@@ -54,7 +56,7 @@ public class EmployeeDao {
 		        return list;  
 		        }});
 		}
-	
+	//This method is used to update the Error table , if the XML is invalid/restricted
 	public int updateErrorTable(String body,String errorReason){
 		StringBuilder updateQuery = new StringBuilder();
 		updateQuery.append("insert into "+'"'+"Error_Table"+'"'+"(Xml_File,Error_Reason,Created_On) values ('"+body+"','"+errorReason+"',current_timestamp)" );
