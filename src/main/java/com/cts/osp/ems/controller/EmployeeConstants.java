d03e0a6b6bcadeaee326db3fd4ddbd81c8267974
Sathish-Periyasamy <p.sathish@outlook.com>
2017-04-03 15:03:25 +0530

    Update EmployeeConstants.java

--- src/main/java/com/cts/osp/ems/controller/EmployeeConstants.java
+++ src/main/java/com/cts/osp/ems/controller/EmployeeConstants.java
@@ -3,10 +3,14 @@ package com.cts.osp.ems.controller;
 public class EmployeeConstants {
 	
 
-	public static final String GET_ALL_EMP = "/rest/emps";
-	public static final String EMP_URI="http://localhost:8080/ems/rest/emps";
+	public static final String SAVE_EMP = "/rest/saveemployee";
+	public static final String EMP_URI="http://localhost:8080/ems/rest/saveemployee";
 	public static final String INVALID_XML ="INVALID XML";
 	public static final String RESTRICTED_XML="RESTRICTED_XML";
 	public static final String XSD_PATH="/Employee.xsd";
+	public static final String SAVED_SUCCESS="Payload saved Successfully";
+	public static final String UPDATED_SUCCESS="Payload updated Successfully";
+	public static final String RESTRICTED_RESPONSE="XML is restricted and saved to Error Table";
+	public static final String INVALID_RESPONSE="XML is invalid and saved to Error Table";
 
 }
