d8d8977539b91e99ac6eb719ae213d512b683a4a
Sathish-Periyasamy <p.sathish@outlook.com>
2017-04-03 15:20:17 +0530

    Update EmployeeConstants.java

--- src/main/java/com/cts/osp/ems/controller/EmployeeConstants.java
+++ src/main/java/com/cts/osp/ems/controller/EmployeeConstants.java
@@ -12,5 +12,9 @@ public class EmployeeConstants {
 	public static final String UPDATED_SUCCESS="Payload updated Successfully";
 	public static final String RESTRICTED_RESPONSE="XML is restricted and saved to Error Table";
 	public static final String INVALID_RESPONSE="XML is invalid and saved to Error Table";
+	public static final String TOTAL_RESP_TIME ="Total Response Time";
+	public static final String STATUS ="status";
+	public static final String MESSAGE ="message";
+	public static final String STATUS_VAL ="Ok";
 
 }
